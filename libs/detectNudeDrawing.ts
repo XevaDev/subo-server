b45f2c778c8064428770e793f507b80ce159e41a
Xevaltan <103420547+XevaDev@users.noreply.github.com>
2022-05-27 11:55:08 +0200

    Update detectNudeDrawing.ts

--- libs/detectNudeDrawing.ts
+++ libs/detectNudeDrawing.ts
@@ -8,7 +8,7 @@ export async function detectNudeDrawing(url: string) {
     headers: {
       "content-type": "application/json",
       "X-RapidAPI-Host": "nsfw-image-classification1.p.rapidapi.com",
-      "X-RapidAPI-Key": "233aeae5ecmshf31da3d9ab8277ap1ddf5bjsn4850f50f1ebc",
+      "X-RapidAPI-Key": "KEY",
     },
     data: `{"url":"${url}"}`,
   };
